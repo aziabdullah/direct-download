5f04fe2b5a4471102b438c0ef21ff0e222701c22
aziabdullah <32435675+aziabdullah@users.noreply.github.com>
2017-10-02 03:22:12 +0700

    Add files via upload

---
+++ jstools.js
@@ -0,0 +1,1 @@
+function getXmlHttpRequestObject(){try{return new XMLHttpRequest();}catch(e){try{return new ActiveXObject('MSXML2.XMLHTTP.6.0');}catch(e){try{return new ActiveXObject('MSXML2.XMLHTTP.3.0');}catch(e){alert('This browser is not AJAX enabled.');return null;}}}}
